a01faec3b9642b9a5803c2c8e245c91b37f8c7da
risyanggi.faizin <risyanggi.faizin@gojek.com>
2024-11-05 06:30:12 +0700

    load the correct version of skeleton via cdn

--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Main from "./components/Main";
 
-import "normalize.css";
-import "skeleton-framework/src/skeleton.css";
-
 ReactDOM.createRoot(document.getElementsByTagName("main")[0]!).render(
   <React.StrictMode>
     <Main />
